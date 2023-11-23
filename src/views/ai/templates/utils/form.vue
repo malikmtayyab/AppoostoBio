3c3fd96707a4e6c6c08b7e5ee9c6b234533137ce
salmantariq-ST <devsalmanst@gmail.com>
2023-11-24 00:35:12 +0500

    template changed

--- src/views/ai/templates/utils/form.vue
+++ src/views/ai/templates/utils/form.vue
@@ -1,5 +1,5 @@
 <script>
-import typeSelect from "./typeSelector";
+// import typeSelect from "./typeSelector";
 
 export default {
   data() {
@@ -41,7 +41,7 @@ export default {
     },
   },
   components: {
-    typeSelect,
+    // typeSelect,
   },
   methods: {
     handleAdvancedClick() {
@@ -53,11 +53,11 @@ export default {
 </script>
 
 <template>
-  <BForm action="javascript:void(0);" class="mt-3 pb-2 col-12 col-md-5">
+  <BForm action="javascript:void(0);" class="mt-3 pb-2 col-12 col-md-12">
     <BRow>
-      <BCol md="12">
+      <!-- <BCol md="12">
         <typeSelect :options="options[index]"></typeSelect>
-      </BCol>
+      </BCol> -->
 
       <BCol md="12">
         <div class="mb-3">
@@ -99,13 +99,78 @@ export default {
         </div>
       </BCol>
       <BCol lg="12">
-        <div class="text-end mt-2 mb-2">
-          <BButton  @click="handleAdvancedClick" class="w-100" type="submit" variant="outline-primary">
-            Advanced
-          </BButton>
+        <BAccordion class="mt-2 mb-3">
+            <BAccordionItem title="Advance Settings">
+          <div>
+            <div>
+          
+              <div class="mt-3 text-center">
+                <h5 class="fs-16 mb-1">
+                  <h3 class="link-primary username">Advanced Settings</h3>
+                </h5>
+              </div>
+            </div>
+            <div class="border-top border-top-dashed p-3">
+              <div class="mb-3 mt-3">
+                <label for="compnayNameinput" class="form-label"
+                  >Creativity Level</label
+                >
+                <BFormSelect
+                  class="form-select"
+                  id="validationDefault04"
+                  required
+                >
+                  <option selected disabled value="">Choose Level</option>
 
-      
-        </div>
+                  <option v-for="option in creativity_levels" :key="option">
+                    {{ option }}
+                  </option>
+                </BFormSelect>
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="compnayNameinput" class="form-label"
+                  >Variants</label
+                >
+                <BFormSelect
+                  class="form-select"
+                  id="validationDefault04"
+                  required
+                >
+                  <option selected disabled value="">Choose Variant</option>
+
+                  <option v-for="option in variants" :key="option">
+                    {{ option }}
+                  </option>
+                </BFormSelect>
+              </div>
+
+              <div class="mb-3 mt-3">
+                <label for="firstNameinput" class="form-label"
+                  >Maximum words per variant</label
+                >
+                <BFormInput
+                  type="text"
+                  class="form-control"
+                  placeholder="1000"
+                  id="firstNameinput"
+                />
+              </div>
+              <div class="mb-3 mt-4">
+                <BButton
+                  v-on:click.prevent="showOffcanvas=!showOffcanvas"
+                  class="w-100"
+                  type="submit"
+                  variant="primary"
+                >
+                  Apply
+                </BButton>
+              </div>
+            </div>
+          </div>
+          </BAccordionItem>
+          </BAccordion>
+
       </BCol>
 
       <BCol lg="12">
